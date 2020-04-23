2dc3f85673a94eb1c4e8445f928bcb6b28c91796
Diogo Monteiro <diogo.arm@gmail.com>
2020-04-23 12:26:52 -0300

    configures the h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS documents;
+
+CREATE TABLE documents (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  subject VARCHAR(100) NOT NULL,
+  body VARCHAR(250) NOT NULL,
+  createdAt TIMESTAMP NOT NULL,
+  updatedAt TIMESTAMP
+);
+
+INSERT INTO documents (title, subject, body, createdAt) VALUES
+  ('title 1 Industrialist', 'subject test abc-123', 'Lorem ipsum Industrialist', '2020-04-23 12:24:00'),
+  ('title 2 Tech Entrepreneur', 'subject test abc-210', 'Lorem ipsum Tech Entrepreneur', '2019-01-01 08:00:02'),
+  ('title 3 Oil', 'subject test xxx-111', 'Lorem ipsum Oil', '2020-01-20 17:14:01');
